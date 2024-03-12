4b45846ac27f5fd50304a6729910a0b99f696291
TheBitStick <the@bitstick.rip>
2024-03-12 14:09:30 -0500

    Darwin Modules on NixOS don't work, who would've thunk it?

--- flake.nix
+++ flake.nix
@@ -3,14 +3,23 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       "pomu" = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           ./configuration.nix
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.admin = import ./home.nix;
+          }
         ];
       };
     };
