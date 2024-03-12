7feba3cb0079b669a87a01f270b39e2fa046187b
TheBitStick <the@bitstick.rip>
2024-03-12 13:58:15 -0500

    Home Manager doesn't work??

--- flake.nix
+++ flake.nix
@@ -3,23 +3,14 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
       "pomu" = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           ./configuration.nix
-	  home-manager.darwinModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.admin = import ./home.nix;
-	  }
         ];
       };
     };
